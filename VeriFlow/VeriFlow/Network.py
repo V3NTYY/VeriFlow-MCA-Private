a68417f35c9c604302d500a040c2cb4bf8982bbc
V3NTYY <165188660+V3NTYY@users.noreply.github.com>
2025-04-12 13:15:02 -0500

    Update Network.py with original, add bool return

--- VeriFlow/VeriFlow/Network.py
+++ VeriFlow/VeriFlow/Network.py
@@ -5,7 +5,6 @@ from VeriFlow.Switch import Switch
 from VeriFlow.Host import Host
 from VeriFlow.Rule import Rule
 from VeriFlow.TrieNode import TrieNode
-from VeriFlow.NetworkError import NetworkError
 
 
 class Network(object):
@@ -19,201 +18,188 @@ class Network(object):
 	def parseNetworkFromFile(self, fileName):
 		f = open(fileName, 'r')
 		try:
-			f.readline()
-			self.addSwitchesFromFileFormat(f)
-			self.addHostsFromFileFormat(f)
-			self.addRulesFromFileFormat(f)
-			f.close()
+			f.readline();
+			self.addSwitchesFromFileFormat(f);
+			self.addHostsFromFileFormat(f);
+			self.addRulesFromFileFormat(f);
+			f.close();
 		except Exception as e: 
-			print(e)
-			f.close()
+			print(e);
+			f.close();
 
 	def getNumberOfECs(self):
-		return self.trie.numberOfECs()
+		return self.trie.numberOfECs();
 
 	def addRulesFromFileFormat(self, f):
-		currentLine = f.readline()
+		currentLine = f.readline();
 		while (not currentLine.startswith("E")):
 			currentLine = currentLine.strip('\n')
-			ecs = self.addRuleFromString(currentLine)
-			currentLine = f.readline()
+			ecs = self.addRuleFromString(currentLine);
+			currentLine = f.readline();
 
 	def addRuleFromString(self, ruleString):
-		print("Adding Rule from string...")
-		print(ruleString)
-		split1 = ruleString.split("-")
-		rule = Rule()
-		rule.setSwitchId(split1[0])
-		rule.setPrefix(split1[1])
-		rule.setNextHopId(split1[2])
-		return self.addRule(rule)
+		split1 = ruleString.split("-");
+		rule = Rule();
+		rule.setSwitchId(split1[0]);
+		rule.setPrefix(split1[1]);
+		rule.setNextHopId(split1[2]);
+		return self.addRule(rule);
 
 	def addHostsFromFileFormat(self, fh):
-		currentLine = fh.readline()
+		currentLine = fh.readline();
 		while (not currentLine.startswith("R")):
-			ts = Host()
+			ts = Host();
 			currentLine = currentLine.strip('\n')
-			split1 = currentLine.split(":")
-			ts.setId(split1[0])
-			ts.setSwitchId(split1[1])
-			switch1 = self.switches.get(ts.getSwitchId())
-			switch1.addConnectedHost(ts.getId())
-			self.hosts[ts.getId()] = ts
-			currentLine = fh.readline()
+			split1 = currentLine.split(":");
+			ts.setId(split1[0]);
+			ts.setSwitchId(split1[1]);
+			switch1 = self.switches.get(ts.getSwitchId());
+			switch1.addConnectedHost(ts.getId());
+			self.hosts[ts.getId()] = ts;
+			currentLine = fh.readline();
 
 	def addSwitchesFromFileFormat(self, fs):
-		currentLine = fs.readline()
+		currentLine = fs.readline();
 		while (not currentLine.startswith("H")):
-			ts = Switch()
+			ts = Switch();
 			currentLine = currentLine.strip('\n')
-			split1 = currentLine.split(":")
-			ts.setId(split1[0])
-			nhops = split1[1].split(",")
+			split1 = currentLine.split(":");
+			ts.setId(split1[0]);
+			nhops = split1[1].split(",");
 			for nh in nhops:
 				ts.addNextHop(nh)
-			self.switches[ts.getId()] = ts
-			currentLine = fs.readline()
+			self.switches[ts.getId()] = ts;
+			currentLine = fs.readline();
 
 
 	def getSwitches(self):
-		return self.switches
+		return self.switches;
 
 	def getHostWithId(self, id):
-		return self.hosts.get(id)
+		return self.hosts.get(id);
 
 	def getHosts(self):
-		return self.hosts
+		return self.hosts;
 
 	def addEc(self, ec):
-		self.getEcs().add(ec)
+		self.getEcs().add(ec);
 	
 
 	def checkWellformedness(self, ecs = None):
-		success = True
-		print("")
-		print("Checking WellFormed-Ness")
-		if(ecs is None or len(ecs) == 0): #From ecs is None
-			print("ECs is None")
+		Success = True
+		if(ecs is None):
 			ecs = self.getECsFromTrie()
-		print("ECs", ecs)
-		print("Hosts", self.hosts.keys())
-		self.getNetworkErrors().clear()
-		print("Clearing Network Errors")
-		for ec in ecs:
-			print("Checking EC", ec)
-			for host in self.hosts.values():
-				print("Checking host", host.getId())
-				connectedSwitchId = host.getSwitchId()
-				print("Connected Switch", connectedSwitchId)
-				currentSwitch = self.switches.get(connectedSwitchId)
-				print("Current Switch", currentSwitch)
-				forwardingGraph = ForwardingGraph()
-				print("Forwarding Graph New", forwardingGraph)
-				forwardingGraph.addToGraph(connectedSwitchId)
-				print("Forwarding Graph Add Switch", forwardingGraph)
+		self.getNetworkErrors().clear();
+		for ec in self.ecs:
+			for host in hosts.values():
+				connectedSwitchId = host.getSwitchId();
+				currentSwitch = self.switches.get(connectedSwitchId);
+				forwardingGraph = ForwardingGraph();
+				forwardingGraph.addToGraph(connectedSwitchId);
 				while (True):
-					## If the current switch doesn't exist, then we have iterated enough
-					if currentSwitch == None:
-						return success
+					associatedRule = currentSwitch.getAssociatedRule(ec.getLeft());
+					if (associatedRule == None) :
+						# networkError = NetworkError();
+						# networkError.setErrorType(ErrorType.BLACK_HOLE);
+						# networkError.setEc(ec);
+						# networkError.setStartingPoint(self.switches.get(connectedSwitchId));
+						# getNetworkErrors().add(networkError);
+						break;
 					
-					## verification logic
-					associatedRule = currentSwitch.getAssociatedRule(ec.getLeft())
-					print("Associated Rule to switch ", currentSwitch, " ", associatedRule)
-					if (associatedRule == None):
-						## Do nothing for black holes for now
-						break
-					nextHopId = associatedRule.getNextHopId()
-					print("Next hop for rule", nextHopId)
+					nextHopId = associatedRule.getNextHopId();
+					if (hosts.containsKey(nextHopId)):
+						break;
 					if (forwardingGraph.contains(nextHopId)):
-						success = False
-						print("LOOP FOUND")
-						print("Forwarding graph contains", nextHopId)
-						networkError = NetworkError()
-						networkError.setErrorType(0) #ErrorType.LOOP
-						networkError.setEc(ec)
-						networkError.setStartingPoint(self.switches.get(connectedSwitchId))
-						self.getNetworkErrors().append(networkError) # .add to .append
-						break
-					currentSwitch = self.switches.get(nextHopId)
-					print("Next switch is now current switch")
-					print("Current switch", currentSwitch)
-					forwardingGraph.addToGraph(nextHopId)
-					print("Forwarding Graph after added next hop", forwardingGraph)
-				return success
+						networkError = NetworkError();
+						networkError.setErrorType(ErrorType.LOOP);
+						networkError.setEc(ec);
+						networkError.setStartingPoint(self.switches.get(connectedSwitchId));
+						getNetworkErrors().add(networkError);
+						Success = False
+						break;
+					currentSwitch = self.switches.get(nextHopId);
+					forwardingGraph.addToGraph(nextHopId);
+		return Success
 
 	def getEcs(self):
-		return self.ecs
+		return ecs;
 
 	def setEcs(self, ecs):
-		self.ecs = ecs
+		self.ecs = ecs;
 	
+
 	def getNetworkErrors(self):
-		return self.networkErrors
+		return self.networkErrors;
 	
+
 	def setNetworkErrors(self, networkErrors):
-		self.networkErrors = networkErrors
+		self.networkErrors = networkErrors;
 	
+
 	def getTrie(self):
-		return self.trie
+		return self.trie;
 	
+
 	def setTrie(self, trie):
-		self.trie = trie
+		self.trie = trie;
 	
+
 	def addRule(self, rule):
-		switch1 = self.switches.get(rule.getSwitchId())
-		switch1.addRule(rule)
+		switch1 = self.switches.get(rule.getSwitchId());
+		switch1.addRule(rule);
 
 		if (rule.getPrefix() not in self.rulePrefixes.keys()):
-			updateEcs = self.getTrie().addRule(rule)
+			updateEcs = self.getTrie().addRule(rule);
 			self.rulePrefixes[rule.getPrefix()]=1
-			return updateEcs
+			return updateEcs;
 		else:
 			self.rulePrefixes[rule.getPrefix()] = self.rulePrefixes.get(rule.getPrefix()) + 1
 			return set()
 
+		
+	
+
 	def deleteRule(self, rule):
-		switchId = rule.getSwitchId()
-		count = self.rulePrefixes.get(rule.getPrefix())
+		switchId = rule.getSwitchId();
+		count = self.rulePrefixes.get(rule.getPrefix());
 		try:
-			self.rulePrefixes.pop(rule.getPrefix())
-			affectedECs = self.trie.deleteFromTrie(rule.getPrefix() + "*")
+			self.rulePrefixes.pop(rule.getPrefix());
+			affectedECs = self.trie.deleteFromTrie(rule.getPrefix() + "*");
 			if (count != 1):
-				self.rulePrefixes.put(rule.getPrefix(), count - 1)
-			switch1 = self.switches.get(switchId)
-			rules = switch1.getRules()
+				self.rulePrefixes.put(rule.getPrefix(), count - 1);
+			switch1 = self.switches.get(switchId);
+			rules = switch1.getRules();
 			for r in rules:
 				if r.equals(rule):
 					rules.remove(r)
 
 			self.switches[switchId].setRules(rules)
-			return affectedECs
+			return affectedECs;
 			
 		except Exception as e:
 			return set()
 	
+
 	def log(self, affectedEcs):
-		print("Number of ECs: " ,self.getNumberOfECs())
-		print("Number of affected ECs: " ,len(affectedEcs))
+		print("Number of ECs: " ,self.getNumberOfECs());
+		print("Number of affected ECs: " ,len(affectedEcs));
 		if (len(self.getNetworkErrors()) == 0):
-			print("Network is well-formed (No property violations)")
+			print("Network is well-formed (No property violations)");
 		else:
 			for  netErr in self.getNetworkErrors():
-				if (netErr.getErrorType() == 0): #ErrorType.LOOP
-					print("There is a loop starting from switch " + netErr.getStartingPoint().getId() + "!")
-				if (netErr.getErrorType() == 1): #ErrorType.BLACK_HOLE
-					print("There is a Black Hole At " + netErr.getStartingPoint().getId() + "!")
+				if (netErr.getErrorType() == ErrorType.LOOP):
+					print("There is a loop starting from switch " + netErr.getStartingPoint().getId() + " for EC " + netErr.getEc() + "!");
 
 	def deleteRuleFromString(self, ruleString):
-		split1 = ruleString.split("-")
-		rule = Rule()
-		rule.setSwitchId(split1[0])
-		rule.setPrefix(split1[1])
-		rule.setNextHopId(split1[2])
-		return self.deleteRule(rule)
+		split1 = ruleString.split("-");
+		rule = Rule();
+		rule.setSwitchId(split1[0]);
+		rule.setPrefix(split1[1]);
+		rule.setNextHopId(split1[2]);
+		return self.deleteRule(rule);
 
 	def generateECs(self):
-		return self.trie.generateECs()
+		return self.trie.generateECs();
 
 	def getECsFromTrie(self):
-		return self.trie.getECListFromTrie()
-
+		return self.trie.getECListFromTrie();
