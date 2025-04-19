35d192ade8b72049f32edc2255128e7026a7dc12
V3NTYY <165188660+V3NTYY@users.noreply.github.com>
2025-04-19 16:05:27 -0500

    Add libOF and __init__ for custom editing

--- RunPoxSendFlows.sh
+++ RunPoxSendFlows.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cp sendflows.py ~/pox/pox/
+cp pox/sendflows.py ~/pox/pox/
+cp pox/__init__.py ~/pox/pox/openflow/
+cp pox/libopenflow_01.py ~/pox/pox/openflow/
 cd ~/pox
 ./pox.py log.level --DEBUG sendflows forwarding.l2_learning openflow.discovery openflow.of_01 --address=127.0.0.1 --port=6653
