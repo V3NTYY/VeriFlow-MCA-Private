f5c1cfa7f52cec1c4b40baf48457c2d3fcd4558b
V3NTYY <165188660+V3NTYY@users.noreply.github.com>
2025-04-19 18:48:18 -0500

    Add _handle to sendflows

--- pox/sendflows.py
+++ pox/sendflows.py
@@ -32,6 +32,21 @@ class _OpenFlowMessageHandler(object):
     def _handle_Unknown(self, event):
         log.debug("Unknown event received from CCPDN: {}".format(event.ofp))
 
+    def _handle(self, connection, msg):
+        log.debug("Received message from CCPDN: {}".format(msg))
+        if msg.type == of.OFP_TYPE_FLOW_MOD:
+            self._handle_FlowMod(msg)
+        elif msg.type == of.OFP_TYPE_FLOW_REMOVED:
+            self._handle_FlowRemoved(msg)
+        elif msg.type == of.OFP_TYPE_STATS_REQUEST:
+            self._handle_StatsRequest(msg)
+        elif msg.type == of.OFP_TYPE_STATS_REPLY:
+            self._handle_StatsReply(msg)
+        elif msg.type == of.OFP_TYPE_FEATURES_REPLY:
+            self._handle_FeaturesReply(msg)
+        else:
+            self._handle_Unknown(msg)
+
 def launch():
     core.registerNew(_OpenFlowMessageHandler)
     log.info("POX/CCPDN controller script loaded and listening for packets...")
