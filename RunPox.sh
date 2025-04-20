a28a429672b51497c800d1a430c50e97e49428f6
V3NTYY <165188660+V3NTYY@users.noreply.github.com>
2025-04-20 16:41:55 -0500

    Fix typo

--- RunPox.sh
+++ RunPox.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp FlowInteface.py ~/pox/pox/
+cp FlowInterface.py ~/pox/pox/
 
 cd ~/pox
 ./pox.py log.level --DEBUG FlowInterface forwarding.l2_learning openflow.discovery openflow.of_01 --address=127.0.0.1 --port=6653
