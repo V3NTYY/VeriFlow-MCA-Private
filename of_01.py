1acaaf0302be4d73a7bb16a0af71b44e3a7e3302
V3NTYY <165188660+V3NTYY@users.noreply.github.com>
2025-04-20 14:13:57 -0500

    Remove of_01 statsreq method not working

--- of_01.py
+++ of_01.py
@@ -173,13 +173,6 @@ class DefaultOpenFlowHandlers (OpenFlowHandlers):
   Connections.
   """
   @staticmethod
-  def handle_STATS_REQUEST (con, msg):
-    log.debug("Got stats request: %s", msg)
-    e = con.ofnexus.raiseEventNoErrors(StatsRequest, con, msg)
-    if e is None or e.halt != True:
-      con.raiseEventNoErrors(StatsRequest, con, msg)
-
-  @staticmethod
   def handle_STATS_REPLY (con, msg):
     e = con.ofnexus.raiseEventNoErrors(RawStatsReply, con, msg)
     if e is None or e.halt != True:
