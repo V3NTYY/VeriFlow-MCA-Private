12ae383040c4600150f85057dac144730162442a
V3NTYY <165188660+V3NTYY@users.noreply.github.com>
2025-04-20 01:02:47 -0500

    Create tcpdump parsing functions for output

--- MCA_VeriFlow/TCPAnalyzer.h
+++ MCA_VeriFlow/TCPAnalyzer.h
@@ -1,8 +1,6 @@
 #ifndef TCPANALYZER_H
 #define TCPANALYZER_H
 
-#include "Log.h"
-
 #include <string>
 #include <fstream>
 #include <iostream>
@@ -14,11 +12,46 @@
 
 typedef std::vector<uint8_t> packet;
 
+// Function to split a string into a vector of words based on delimiters
+std::vector<std::string> splitInput(std::string input, std::vector<std::string> delimiters) {
+    std::vector<std::string> words;
+    std::string word;
+
+    // If the line is empty, return early
+    if (input.empty()) {
+		return words;
+	}
+
+    // Iterate through the entire input string. If a delimiter is found, the first half is added as a word. The second half is then checked for delimiters again.
+    for (size_t i = 0; i < input.length(); i++) {
+		bool isDelimiter = false;
+		for (std::string delimiter : delimiters) {
+			if (input.substr(i, delimiter.length()) == delimiter) {
+				isDelimiter = true;
+				if (!word.empty()) {
+					words.push_back(word);
+					word.clear();
+				}
+				break;
+			}
+		}
+		if (!isDelimiter) {
+			word += input[i];
+            // If this is the last word, push it back
+            if (i == input.length() - 1) {
+                words.push_back(word);
+            }
+		}
+	}
+
+    return words;
+}
+
 class TCPAnalyzer {
 	public:
 		// Thread method
 		void thread(bool *run, std::string controllerPort) {
-			loggy << "[CCPDN]: Starting TCPDump thread...\n";
+			loggy << "\n\n[CCPDN]: Starting TCPDump thread...\n";
 			while (*run) {
 				runTCPDump(controllerPort, run);
 			}
@@ -27,7 +60,7 @@ class TCPAnalyzer {
 		void runTCPDump(std::string port, bool *run) {
 			// Construct the tcpdump command
 			std::string pidCommand = "echo $! > /tmp/ccpdn_xterm_pid";
-			std::string sysCommand = "xterm -hold -e \"sudo tcpdump -v -i lo tcp port " + port + " -l -n\" & " + pidCommand;
+			std::string sysCommand = "xterm -hold -e \"sudo tcpdump -v -i lo tcp port " + port + " -l -n  > /dev/null 2>&1\" & " + pidCommand;
 
 			#ifdef __unix__
 			// Open a pipe to read the output of tcpdump
@@ -37,16 +70,14 @@ class TCPAnalyzer {
 				throw std::runtime_error("Failed to run tcpdump: " + std::string(strerror(errno)));
 			}
 
-			char buffer[1024];
-			while ((fgets(buffer, sizeof(buffer), pipe) != nullptr) && (*run)) {
-				// Process each line of tcpdump output
-				std::string packetData(buffer);
-				packet newPacket = analyzePacket(packetData);
+			std::string readBuffer;
+			char chunk[1024];
+			while ((fgets(chunk, sizeof(chunk), pipe) != nullptr) && (*run)) {
+				// Process our continuous stream of bytes
+				readBuffer.append(chunk);
 
-				// Add our packet if it qualifies under analyzePacket
-				if (!newPacket.empty()) {
-					this->buffer.push_back(newPacket);
-				}
+				// Process the buffer
+				processBuffer(readBuffer);
 			}
 
 			// Close the pipe
@@ -54,18 +85,80 @@ class TCPAnalyzer {
 			#endif
 		}
 
+		size_t findTimestamp(const std::string& buffer, size_t compare) {
+			// Look for the first occurrence of a colon, FORMAT: [hour:minute:second], 24:00:00.000000
+			size_t pos = buffer.find_first_of("0123456789:");
+
+			size_t comparePos = compare;
+
+			while (pos != std::string::npos) {
+				// Check if the next characters match the timestamp format
+				// If we are using "star
+				if ((isdigit(buffer[pos])) && (buffer[pos + 2] == ':') && (buffer[pos + 5] == ':') && (pos != comparePos)) {
+					return pos; // Found a timestamp
+				}
+				pos = buffer.find_first_of("0123456789:", pos + 1); // Continue searching
+			}
+			return std::string::npos; // No timestamp found
+		}
+
+		void processBuffer(std::string& buffer) {
+			// Identify start of packet through timestamp
+			size_t start = findTimestamp(buffer, -1);
+			if (start == std::string::npos) { // wait until we have more data
+				return;
+			}
+
+			// Identify end of packet by the start of next timestamp, the previous
+			size_t end = findTimestamp(buffer, start) - 1;
+			if (start == std::string::npos) { // wait until we have more data
+				return;
+			}
+
+			// Extract the packet data from the buffer
+			std::string packetData = buffer.substr(start, end - start + 1);
+			buffer.erase(0, end + 1); // Remove the processed packet from the buffer
+
+			// Process the packet data
+			packet newPacket = analyzePacket(packetData);
+			if (!newPacket.empty()) {
+				this->buffer.push_back(newPacket); // Add the packet to the buffer
+			}
+		}
+
 		packet analyzePacket(const std::string& packetData) {
 
 			// Print the packet data for dbg
 			loggy << "Captured Packet: " << packetData << std::endl;
 
+			// Find start of OpenFlow section
+			size_t start = packetData.find("OpenFlow");
+
 			// Check if the packet is an OpenFlow packet
-			if (packetData.find("OpenFlow") != std::string::npos) {
-				// Extract the relevant data from the packet
-				packet newPacket(packetData.begin(), packetData.end());
-				return newPacket;
+			if (start == std::string::npos) {
+				return packet();
 			}
-			// If not an OpenFlow packet, return an empty packet
+
+			// Extract the relevant data from the packet
+			std::string splicedData = packetData.substr(start);
+
+			// Trim all newlines, tabs, and spaces from the string
+			splicedData.erase(std::remove(splicedData.begin(), splicedData.end(), '\n'), splicedData.end());
+			splicedData.erase(std::remove(splicedData.begin(), splicedData.end(), '\t'), splicedData.end());
+			splicedData.erase(std::remove(splicedData.begin(), splicedData.end(), ' '), splicedData.end());
+			splicedData.erase(std::remove(splicedData.begin(), splicedData.end(), '\r'), splicedData.end());
+
+			// Split the spliced data into words by using a comma as a delimiter
+			std::vector<std::string> words = splitInput(splicedData, { "," });
+
+			// For now, print this data to log for debugging
+			loggy << "Spliced Data: " << splicedData << std::endl;
+			loggy << "Words: ";
+			for (const auto& word : words) {
+				loggy << word << " ";
+			}
+			loggy << std::endl;
+
 			return packet();
 		}
 
