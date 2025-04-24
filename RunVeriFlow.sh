ac3e770dc6766666a280ec6dbd690398923090e5
V3NTYY <165188660+V3NTYY@users.noreply.github.com>
2025-04-23 21:05:44 -0500

    Try different logic than expect

--- RunVeriFlow.sh
+++ RunVeriFlow.sh
@@ -6,18 +6,8 @@ PORT=${PORT:-6636}
 read -p "Enter the location of the topology file (default is ../SingleTop.topo): " TOP
 TOP=${TOP:-"../SingleTop.topo"}
 
-sudo apt-get update
-sudo apt-get install expect
+address="127.0.0.1"
 
 cd VeriFlow
 
-expect <<EOF
-spawn sudo python3 Main.py
-expect "Enter network configuration file name (eg.: file.txt):"
-send "$TOP\r"
-expect "Enter IP address to host VeriFlow on (i.e. 127.0.0.1)"
-send "127.0.0.1\r"
-expect "Enter port to host VeriFlow on (i.e. 6657)"
-send "$PORT\r"
-interact
-EOF
+sudo python3 Main.py $TOP $address $PORT
